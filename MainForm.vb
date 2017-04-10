933567ed4d930329dc862b4196a7aac912f65018
Thomas Beck <thomas8beck@gmail.com>
2017-04-09 19:18:14 -0500

    Add files via upload

---
+++ MainForm.vb
@@ -0,0 +1,62 @@
+﻿Public Class MainForm
+
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        Dim intResponse As Integer
+
+        'Display a message box asking user if they want to quit
+        intResponse = MessageBox.Show("Do you want to quit?", "Confrim Quit", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1)
+
+        'Ask the user to confirm Exiting the program
+        If intResponse = vbYes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub btnEasternSales_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEasternSales.Click
+        Dim frmEasternReport As New ReportForm
+
+        'Put the word Eastern into the lblRegion label
+        frmEasternReport.lblRegion.Text = "Eastern Regional Sales"
+
+
+        'now show the form
+        frmEasternReport.ShowDialog()
+
+        'If both commissions have been calculated, enable the Total Commission button
+        If lblEasternCommission.Text <> "" And lblWesternCommission.Text <> "" Then
+            btnTotalCommission.Enabled = True
+
+        End If
+
+    End Sub
+
+    Private Sub btnWesternSales_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnWesternSales.Click
+        Dim frmWesternReport As New ReportForm
+
+        'Put the word Eastern into the lblRegion label
+        frmWesternReport.lblRegion.Text = "Western Regional Sales"
+
+
+        'now show the form
+        frmWesternReport.ShowDialog()
+
+        'If both commissions have been calculated, enable the Total Commission button
+        If lblWesternCommission.Text <> "" And lblEasternCommission.Text <> "" Then
+            btnTotalCommission.Enabled = True
+        End If
+    End Sub
+
+    
+    Private Sub btnTotalCommission_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTotalCommission.Click
+        Dim dblTotalCommission As Double
+        dblTotalCommission = CDbl(lblEasternCommission.Text) + CDbl(lblWesternCommission.Text)
+
+        'Display the result in the Total Commission label
+        lblTotalComm.Text = dblTotalCommission.ToString("c")
+
+    End Sub
+
+    Private Sub MainForm_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ ReportForm.vb
@@ -0,0 +1,50 @@
+﻿
+Public Class ReportForm
+
+    Private Sub btnReturn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnReturn.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        txtSales.Text = ""
+        txtRate.Text = ""
+        lblCommission.Text = ""
+        'Send focus to the txtSales text box
+        txtSales.Focus()
+
+    End Sub
+
+    Private Sub btnCommission_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCommission.Click
+        Dim dblCommission, dblSales, dblRate As Double
+        Try
+            'copy the input to the variables
+            dblSales = CDbl(txtSales.Text)
+            dblRate = CDbl(txtRate.Text)
+
+            'Compute the commission and display it
+            dblCommission = dblSales * dblRate / 100
+            lblCommission.Text = dblCommission.ToString("c")
+
+            'Now let's copy the results back to the Main Form
+            If lblRegion.Text = "Eastern Regional Sales" Then
+                MainForm.lblEasternCommission.Text = lblCommission.Text
+
+                'Now do this for the Western Regional Sales
+            ElseIf lblRegion.Text = "Western Regional Sales" Then
+                MainForm.lblWesternCommission.Text = lblCommission.Text
+
+            End If
+        Catch
+            MessageBox.Show("You must enter both Sales and Commission Rate")
+            'Send focus to txtSales
+            txtSales.Focus()
+
+        End Try
+    End Sub
+
+    Private Sub ReportForm_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+
+
